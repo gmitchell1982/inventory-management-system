149ec74f19645de1a0ef5dc20dad9231245a4940
Gerald L. Mitchell <gmitchell1982@gmail.com>
2021-07-20 12:24:27 -0400

    create the Users table

--- ims.sql
+++ ims.sql
@@ -2,3 +2,13 @@ create database InventoryManagementSystem;
 
 -----------------------------------
 
+create table InventoryManagementSystem.dbo.Users(
+  UserID int identity(1,1) primary key not null,
+  FirstName varchar(15) not null,
+  MiddleName varchar(15),
+  Surname varchar(25) not null,
+  Status bit not null,
+  Purchaser bit not null
+);
+
+-----------------------------------
